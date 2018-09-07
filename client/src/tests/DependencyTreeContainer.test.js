36711c5ca82ae591de1ba4e7306881e8b532b2c6
Agata Sumowska <agata.sumowska@gmail.com>
2018-09-07 22:12:17 +0100

    displays user message when fetching data

--- client/src/tests/DependencyTreeContainer.test.js
+++ client/src/tests/DependencyTreeContainer.test.js
@@ -7,4 +7,9 @@ describe('DependencyTree', () => {
     fetch.mockResponseOnce(JSON.stringify({"accepts":"~1.3.5","array-flatten":"1.1.1","body-parser":"1.18.2" }))
     shallow(<DependencyTreeContainer />);
   });
+
+  it('renders loading until the data gets back', () => {
+    const wrapper = shallow(<DependencyTreeContainer />, {disableLifecycleMethods: true});
+    expect(wrapper.find('div>div').first().text()).toBe('loading')
+  });
 });
